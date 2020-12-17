2e1ab3f05362f4b39251681ec322324e3ec5eb4f
alarasapa <61461890+alarasapa@users.noreply.github.com>
2020-12-17 19:30:07 +0100

    Reloj terminado

--- WebContent/dataHora.js
+++ WebContent/dataHora.js
@@ -15,25 +15,26 @@
 		var dia = horaActual.getDate();
 		var mes = horaActual.getMonth() + 1;
 		var any = horaActual.getFullYear();
-	
 		
-		document.getElementById("hora").addEventListener("dblclick", function(){
+		if (tipusHora == "ampm"){
+			var ampm;
+			
+			if (horas < 13){
+				ampm = "AM";
+			} else {
+				ampm = "PM";
+				horas = horas - 12;
+			}
+		
+			return horas + ":" + minuts + ":" + segons + " " + ampm + "<br>" +
+				"Aviu és: " + diaSetm + "<br>" +
+				dia + "/" + mes + "/" + any;
+				
+		} else {
 			return horas + ":" + minuts + ":" + segons + "<br>" + 
 				"Avui es " + diaSetm + "<br>" +
 				dia + "/" + mes + "/" + any;
-		})
-		
-		var ampm;
-		if (horas < 13){
-			ampm = "AM";
-		} else {
-			ampm = "PM";
-			horas = horas - 12;
 		}
-	
-		return horas + ":" + minuts + ":" + segons + " " + ampm + "<br>" +
-			"Aviu és: " + diaSetm + "<br>" +
-			dia + "/" + mes + "/" + any;
 	}
 
 	function diaSetmana(numero){
@@ -65,12 +66,25 @@
 		}
 	
 	function actualitzarHora(){
-		document.getElementById("hora").innerHTML = dataHora();		
+		document.getElementById("hora").innerHTML = dataHora();
 	}
 	
+	var tipusHora;
 	function init(){
+		tipusHora = "24h";
+		
+		document.getElementById("hora").addEventListener("dblclick", function(){
+			if (tipusHora == "24h"){
+				tipusHora = "ampm";
+			} else {
+				tipusHora = "24h";
+			}
+			console.log(tipusHora);
+		})
+		
 		//Cridem la funció de mostrar l'hora
 		actualitzarHora();
+		
 		//Creem un interval que cada segon s'actualitzi l'hora
 		setInterval(actualitzarHora, 1000);
 	}
