a38c9f2f6c5bb3dd366d207b34da968110def75f
alarasapa <61461890+alarasapa@users.noreply.github.com>
2020-12-19 16:07:11 +0100

    Modificació de BBDD
    Nos olvidamos de la data de reserva

--- pr02_agust__n_bernat.sql
+++ pr02_agust__n_bernat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-12-2020 a las 16:32:00
+-- Tiempo de generación: 19-12-2020 a las 16:05:14
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.10
 
@@ -20,30 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `pr02_agustín_bernat`
 --
-CREATE DATABASE IF NOT EXISTS `pr02_agustín_bernat` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `pr02_agustín_bernat`;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `continents`
---
-
-CREATE TABLE `continents` (
-  `idContinent` int(10) UNSIGNED NOT NULL,
-  `nom` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `paisos`
---
-
-CREATE TABLE `paisos` (
-  `idPais` int(11) NOT NULL,
-  `nom` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -56,18 +32,28 @@ CREATE TABLE `reserves` (
   `nom` varchar(50) DEFAULT NULL,
   `persones` varchar(4) DEFAULT NULL,
   `telefon` char(9) DEFAULT NULL,
-  `preu` float DEFAULT NULL
+  `preu` float DEFAULT NULL,
+  `data` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `reserves`
+--
+
+INSERT INTO `reserves` (`idReserva`, `nom`, `persones`, `telefon`, `preu`, `data`) VALUES
+(1, 'prova', '1', '123456789', 100, '2020-12-03'),
+(2, 'otrapurbue', '2', '123456789', 200, '2020-12-09'),
+(3, 'qqqqqqqqq', '1', '123456789', 100, '2020-12-10'),
+(4, 'qa', '1', '123456789', 100, '2020-12-10'),
+(5, 'q', '1', '123456789', 100, '2020-12-01'),
+(6, 'cdcd', '1', '123456789', 100, '2020-11-19'),
+(7, 'qqqqeeghtrbrtbvrtr', '1', '123456789', 80, '2020-09-17'),
+(8, '11q1q1', '1', '123456789', 100, '2020-12-30'),
+(9, 'otraaaaaa', '1', '123456789', 80, '2020-12-02');
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `continents`
---
-ALTER TABLE `continents`
-  ADD PRIMARY KEY (`idContinent`);
 
 --
 -- Indices de la tabla `reserves`
@@ -80,16 +66,10 @@ ALTER TABLE `reserves`
 --
 
 --
--- AUTO_INCREMENT de la tabla `continents`
---
-ALTER TABLE `continents`
-  MODIFY `idContinent` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `reserves`
 --
 ALTER TABLE `reserves`
-  MODIFY `idReserva` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `idReserva` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
